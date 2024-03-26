4a17bdd3ad2050b9585bbde9ce64bdd78f182e31
songjaehoon <sjh101111@nate.com>
2024-03-26 14:35:24 +0900

    feat swagger ui 적용
    1. SwaggerConfig 스웨거 설정 추가
    2. build.gradle 의존성 추가
    3. CommentController에 swagger어노테이션 추가

--- src/main/java/com/estsoft13/matdori/controller/CommentController.java
+++ src/main/java/com/estsoft13/matdori/controller/CommentController.java
@@ -3,18 +3,27 @@ package com.estsoft13.matdori.controller;
 import com.estsoft13.matdori.dto.AddCommentRequestDto;
 import com.estsoft13.matdori.dto.CommentResponseDto;
 import com.estsoft13.matdori.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "CommentController/댓글 CRUD", description = "댓글 관리 API")
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
 
+
     @GetMapping("/api/comments/{reviewId}")
+    @Operation(summary = "리뷰 상세페이지 댓글 모두 보기", description = "리뷰 상세페이지 댓글 모두 보기")
+//    @ApiResponse(responseCode = "100", description = "요청 성공", content = @Content(mediaType = "application/json"))
+    @Parameter(name = "id", description = "리뷰 상세페이지 ID", example = "1")
     public List<CommentResponseDto> getAllComments(@PathVariable Long reviewId) {
         return commentService.getAllComments(reviewId);
     }
